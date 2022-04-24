e7009151c7e9344d8a39c57450018120d7452777
Johnny Quintero <55515754+JohnnyQuintero16@users.noreply.github.com>
2022-04-24 18:42:35 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var btnConfirm = document.getElementById("modalUsu");
+var btnRegis = document.getElementById("cntPerson");
+var inputInvitao = document.getElementById("floatingInputValue");
+
+inputInvitao.addEventListener('change',()=>{
+    document.getElementById("btnsubmit").disabled=false;
+})
+btnRegis.addEventListener('click', () =>{
+    let data = document.getElementById("cntPersonas").value;
+
+
+})
+
+btnConfirm.addEventListener('show.bs.modal', ()=>{
+
+})
