4fe7666338560997f4721a4b9fb6581461daa385
Johnny Quintero <55515754+JohnnyQuintero16@users.noreply.github.com>
2022-04-24 19:19:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ var inputInvitao = document.getElementById("floatingInputValue");
 inputInvitao.addEventListener('change',()=>{
     document.getElementById("btnsubmit").disabled=false;
 })
+
 btnRegis.addEventListener('click', () =>{
     let data = document.getElementById("cntPersonas").value;
 
-
+    let padre = document.getElementById("bodyCard");
+    let modalUsu = document.getElementById("camposAdd");
+    let fragment = document.createDocumentFragment();
+    for(let i = 1; i <= data; i++){
+        let clone = modalUsu.content.cloneNode(true);
+        clone.querySelector("#idInvitado").innerHTML = "Invitado " + (i+1);
+        fragment.appendChild(clone);
+    }
+    padre.appendChild(fragment);
 })
 
 btnConfirm.addEventListener('show.bs.modal', ()=>{
